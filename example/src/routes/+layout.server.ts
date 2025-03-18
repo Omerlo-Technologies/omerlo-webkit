90ed15c9e1cb75e5089f1d3be65f7c51a2654b53
Alexandre Lepretre <alexandre.lprtr@gmail.com>
2025-03-18 09:16:46 -0400

    Add jsonwebtoken

--- example/src/routes/+layout.server.ts
+++ example/src/routes/+layout.server.ts
@@ -4,6 +4,7 @@ import { loadUserSession } from 'omerlo-webkit/reader/server';
 import { type UserSession } from 'omerlo-webkit/reader';
 
 export const load: PageLoad = async ({ fetch, cookies }) => {
+  console.log('Request page.server.ts');
   const userSession: UserSession = await loadUserSession(fetch, cookies);
 
   return { userSession };
