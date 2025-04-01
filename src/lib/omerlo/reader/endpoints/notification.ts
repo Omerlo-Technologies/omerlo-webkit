220a97c6a5af2aef1e4f504295da9e40ad0a2c95
NitnekB <nitnekb@gmail.com>
2025-04-01 09:00:36 +0200

    feat(reader): list topics

--- src/lib/omerlo/reader/endpoints/notification.ts
+++ src/lib/omerlo/reader/endpoints/notification.ts
@@ -1,11 +1,42 @@
 import { request } from '$reader/utils/request';
+import { parseMany, type ApiAssocs, type ApiData, type PagingParams } from '$reader/utils/api';
 
 export const notificationFetchers = (f: typeof fetch) => {
   return {
+    listTopics: listTopics(f),
     subscribeToTopic: subscribeToTopic(f),
     unsubscribeFromTopic: unsubscribeFromTopic(f),
   };
 };
+
+export function listTopics(f: typeof fetch) {
+  return (params?: Partial<PagingParams>) => {
+    const opts = { queryParams: params, parser: parseMany(parseTopicSummary) };
+    return request(f, '/topics', opts);
+  };
+}
+
+export function parseTopicSummary(data: ApiData, _assocs: ApiAssocs): TopicSummary {
+  return {
+    id: data.id,
+    localized: {
+      locale: data.localized.locale,
+      name: data.localized.name,
+    },
+    available_locales: data.available_locales,
+    updatedAt: data.updated_at
+  };
+}
+
+export interface TopicSummary {
+  id: string,
+  localized: {
+    locale: string,
+    name: string,
+  },
+  available_locales: string[],
+  updatedAt: Date
+}
 
 export interface SubscribtionParams {
   topicId: string;
