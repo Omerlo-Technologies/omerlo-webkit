43fed5dd4dabcf28525f83fed747fac773b2e70a
Noah <noah.nemes-lauzier@drakkar.com>
2025-05-26 14:59:11 -0400

    Added person fetchers and parsing logic

--- src/lib/omerlo/reader/fetchers.ts
+++ src/lib/omerlo/reader/fetchers.ts
@@ -3,6 +3,7 @@ import { categoriesFetchers } from './endpoints/categories';
 import { deviceFetchers } from './endpoints/device';
 import { notificationFetchers } from './endpoints/notification';
 import { oauthFetchers } from './endpoints/oauth';
+import { personFetchers } from './endpoints/person';
 
 export const fetchers = (f: typeof fetch) => {
   return {
@@ -10,6 +11,7 @@ export const fetchers = (f: typeof fetch) => {
     ...deviceFetchers(f),
     ...oauthFetchers(f),
     ...categoriesFetchers(f),
+    ...personFetchers(f),
     notifications: notificationFetchers(f)
   };
 };
