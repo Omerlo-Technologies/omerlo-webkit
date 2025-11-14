89d137b5135aa76e7ef6e6a7245b71291d5e126e
NitnekB <nitnekb@gmail.com>
2025-11-14 13:26:39 +0100

    fix: hot reload example app

--- example/src/routes/+layout.server.ts
+++ example/src/routes/+layout.server.ts
@@ -1,10 +1,9 @@
-import type { PageLoad } from './$types';
+import type { LayoutServerLoad } from './$types';
 
 import { loadUserSession } from 'omerlo-webkit/reader/server';
 import { type UserSession } from 'omerlo-webkit/reader';
 
-export const load: PageLoad = async ({ fetch, cookies }) => {
+export const load: LayoutServerLoad = async ({ fetch, cookies }) => {
   const userSession: UserSession = await loadUserSession(fetch, cookies);
-
   return { userSession };
 };
